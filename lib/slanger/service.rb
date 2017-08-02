a75d52ab82eb8c78bacddf4c2281ab907b0e150f
Samuel Cochran <sj26@sj26.com>
2017-08-02 16:46:45 +1000

    SSL applies to API, too

--- lib/slanger/service.rb
+++ lib/slanger/service.rb
@@ -6,7 +6,12 @@ module Slanger
     def run
       Slanger::Config[:require].each { |f| require f }
       Thin::Logging.silent = true unless Slanger::Config.debug
-      Rack::Handler::Thin.run Slanger::Api::Server, Host: Slanger::Config.api_host, Port: Slanger::Config.api_port
+      Rack::Handler::Thin.run(Slanger::Api::Server, Host: Slanger::Config.api_host, Port: Slanger::Config.api_port) do |server|
+        if Slanger::Config[:tls_options]
+          server.ssl = true
+          server.ssl_options = Slanger::Config[:tls_options]
+        end
+      end
       Slanger::WebSocketServer.run
     end
 
